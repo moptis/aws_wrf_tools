f38d06a8bef64a1cb7965acb77d39293b96e60fb
mike <optism@gmail.com>
2022-07-19 20:45:30 +0000

    add

--- scripts/grib2interm/submit_grib2interm_batch.sh
+++ scripts/grib2interm/submit_grib2interm_batch.sh
@@ -2,38 +2,13 @@
 
 # MAKE SURE THE NUMBER OF SUBMISSIONS MATCHES ntasks ABOVE
 
-
-
-#python grib_to_interm_old.py 2015-01-01 2015-02-01
-#python grib_to_interm_old.py 2018-02-01 2018-03-01
-#python grib_to_interm_old.py 2018-03-01 2018-04-01
-#python grib_to_interm_old.py 2018-04-01 2018-05-01
-
-#y=2018
-
-#python grib_to_interm_old.py 2015-03-01 2015-04-01
-#python grib_to_interm_old.py 2015-02-01 2015-03-01
-python grib_to_interm_old.py 2020-05-01 2020-06-01
-
-#python grib_to_interm_old.py 2018-01-01 2018-02-01
-#python grib_to_interm_old.py 2018-02-01 2018-03-01
-#python grib_to_interm_old.py 2018-03-01 2018-04-01
-#python grib_to_interm_old.py 2018-04-01 2018-05-01
-#python grib_to_interm_old.py 2018-05-01 2018-06-01
-#python grib_to_interm_old.py 2018-06-01 2018-07-01
-#python grib_to_interm_old.py 2018-07-01 2018-08-01
-#python grib_to_interm_old.py 2018-08-01 2018-09-01
-#python grib_to_interm_old.py 2018-09-01 2018-10-01
-#python grib_to_interm_old.py 2018-10-01 2018-11-01
-#python grib_to_interm_old.py 2018-11-01 2018-12-01
-#python grib_to_interm_old.py 2018-12-01 2019-01-01
-
-
-
-#done
-#python grib_to_interm.py 2020-07-01 2020-08-01 &
-#python grib_to_interm.py 2020-08-01 2020-09-01 &
-#python grib_to_interm.py 2020-09-01 2020-10-01 &
-#python grib_to_interm.py 2020-10-01 2020-11-01 &
-#python grib_to_interm.py 2020-11-01 2020-12-01 &
-#python grib_to_interm.py 2020-12-31 2021-01-01 &
+for y in {2018..2021}
+do
+  for m in {01..11}
+  do
+    m_next=$((m+1))
+    python grib_to_interm_old.py $y-$m-01 $y-${m_next}-01
+  done
+  y_next=$((y+1))
+  python grib_to_interm_old.py $y-12-01 ${y_next}-01-01
+done
