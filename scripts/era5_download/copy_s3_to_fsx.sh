6daf0945f8d3feeb851c25fdf4e56be23503f0a1
mike <optism@gmail.com>
2022-07-19 20:48:15 +0000

    s3 to fsx copy

---
+++ scripts/era5_download/copy_s3_to_fsx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#for y in {2018..2021}
+for y in 2021
+do
+  for m in 01
+#  for m in {01..12}
+  do
+    mkdir -p /shared/era5_data/$y/$m/
+    aws s3 cp s3://nwpm/era5_data/interm/$y/$m/ /shared/era5_data/$y/$m --recursive --exclude "*" --include "ERA5_*"
+  done
+done 
