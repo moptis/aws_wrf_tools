7819f0c4c7b4aa77b48c9198050da44f168f43be
AWS ParallelCluster user <ec2-user@ip-10-120-16-250.us-west-2.compute.internal>
2022-11-18 19:20:02 +0000

    fixes

--- scripts/era5_download/copy_s3_to_fsx.sh
+++ scripts/era5_download/copy_s3_to_fsx.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#for y in {2018..2021}
-for y in 2015
+for y in {2018..2021}
 do
-#for m in {01..12}
-  for m in 12
+for m in {01..12}
   do
     mkdir -p /shared/era5_data/$y/$m/
     aws s3 cp s3://nwpm/era5_data/interm/$y/$m/ /shared/era5_data/$y/$m --recursive --exclude "*" --include "ERA5_*"
